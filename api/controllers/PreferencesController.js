13cc3db2ec6b1532bb934eeeb8873400d33db3a6
Goku <tecxnitrom@gmail.com>
2016-02-24 08:38:51 +0530

    temp code added in preference controler

--- api/controllers/PreferencesController.js
+++ api/controllers/PreferencesController.js
@@ -14,5 +14,15 @@ module.exports = {
       }
       return res.render('preferences',{user: user});
 		});
+ },
+ submit: function (req, res) {
+   if(! req.body)
+    return res.send(403)
+
+    // IMAGE CODE
+//    avatarUrl: require('util').format('%s/user/avatar/%s', sails.getBaseUrl(),req.user.username+'_'+sails.acumen.avatar_width+'X'+sails.acumen.avatar_height),
+    // Grab the first file and use it's `fd` (file descriptor)
+//    avatarFd: uploadedFiles[0].fd
+
  }
 };
